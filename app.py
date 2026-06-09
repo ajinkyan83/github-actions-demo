465d2e21a68f937d75c96350dd0277f68c345d90
Ajinkya Nahar <ajinkyanahar@gmail.com>
2026-06-09 12:10:11 +0530

    adding python code

---
+++ app.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+user_input = input()
+
+query = "SELECT * FROM users WHERE id=" + user_input
+
+print(query)
+
+# github-actions-demo
+
+# def main():
+#     print("Hello, World!")
+    
+
+
+# if __name__ == "__main__":
+#     main()
